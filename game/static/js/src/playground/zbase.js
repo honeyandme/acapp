49cd9e5e01c6ec64d690cac03040254ed76f1c4c
维康 张 <2414342539@qq.com>
2022-02-24 21:32:44 +0800

    write menu and playground

---
+++ game/static/js/src/playground/zbase.js
@@ -0,0 +1,18 @@
+class AcGamePlayground{
+    constructor(root){
+        this.root = root;
+        this.$playground = $('<div>游戏界面</div>');
+
+        this.root.$ac_game.append(this.$playground);
+        this.start();
+    }
+    start(){
+
+    }
+    show(){
+        this.$playground.show();
+    }
+    hide(){
+        this.$playground.hide();
+    }
+}
--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,5 +1,12 @@
 class AcGame{
     constructor(id){
-       console.log("create new game!");
+        this.id = id;
+        this.$ac_game = $('#' + id);
+        this.menu = new AcGameMenu(this);
+        this.playground = new AcGamePlayground(this);
+        this.start();
+    }
+    start(){
+
     }
 }
